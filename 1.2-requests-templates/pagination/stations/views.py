b295476fb2b09daef5b25b2c53dbb2adbec61a68
Milka <mail.luda.ru@gmail.com>
2025-09-16 23:36:55 +1000

    Реализована пагинация для списка автобусных остановок

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +10,20 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    # читаем csv
+    with open(settings.BUS_STATION_CSV, encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        stations = list(reader)
+
+    # получаем номер страницы из GET-параметров (?page=2)
+    page_number = int(request.GET.get('page', 1))
+
+    # пагинация (по 10 записей на страницу)
+    paginator = Paginator(stations, 10)
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page.object_list,  # записи текущей страницы
+        'page': page,                      # объект страницы (для next/previous)
     }
     return render(request, 'stations/index.html', context)
