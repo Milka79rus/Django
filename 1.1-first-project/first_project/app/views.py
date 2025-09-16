3d62fd130dff630f07f98032c79b46d238b67ee1
Milka <mail.luda.ru@gmail.com>
2025-09-16 16:46:59 +1000

    Реализован сервис рецептов: view, urls и шаблон

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render, reverse
+from django.shortcuts import render
 from datetime import datetime
+from django.urls import reverse
 import os
 
 
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 DATA = {
     'omlet': {
@@ -16,15 +17,19 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, dish):
+    recipe = DATA.get(dish)  # ищем рецепт по ключу
+    if recipe is None:
+        context = {'recipe': {}}  # пустой словарь => сработает {% empty %}
+    else:
+        servings = int(request.GET.get('servings', 1))  # по умолчанию 1
+        context = {
+            'recipe': {
+                ingredient: amount * servings
+                for ingredient, amount in recipe.items()
+            }
+        }
+    return render(request, 'calculator/index.html', context)
