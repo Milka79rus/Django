cc793776de19d889d22e47c8721aaf1fd0832c1b
Milka <mail.luda.ru@gmail.com>
2025-09-19 00:47:12 +1000

    Завершена реализация каталога телефонов: модели, views, urls, шаблоны, импорт CSV

--- 2.1-databases/work_with_database/main/urls.py
+++ 2.1-databases/work_with_database/main/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -22,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', phones.views.index),
     path('catalog/', phones.views.show_catalog, name='catalog'),
-    path('catalog/<slug:slug>/', phones.views.show_product, name='phone'),
+    path('catalog/<slug:slug>/', phones.views.show_product, name='phone_detail'),
 ]
--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,31 @@
 import csv
-
+from datetime import datetime
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from phones.models import Phone
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    help = 'Выгрузка каталога телефонов из CSV и сохранение в базу данных'
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open('phones.csv', 'r', encoding='utf-8') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            release_date = datetime.strptime(phone['release_date'], '%Y-%m-%d').date()
+            slug_value = slugify(phone['name'])
+
+            Phone.objects.update_or_create(
+                id=int(phone['id']),
+                defaults={
+                    'name': phone['name'],
+                    'price': float(phone['price']),
+                    'image': phone['image'],
+                    'release_date': release_date,
+                    'lte_exists': phone['lte_exists'].lower() == 'true',
+                    'slug': slug_value,
+                },
+            )
+
+        self.stdout.write(self.style.SUCCESS(f'Импортировано {len(phones)} телефонов'))
